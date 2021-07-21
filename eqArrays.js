f5dba8a4cfab4f35169eab2c28358f5003e589c2
Amit Baid <baidstar@hotmail.com>
2021-07-20 19:06:11 -0600

    completed the eqArray loetide assignment. Test if 2 arrays are equal

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+// Create a function that takes in two arrays and checks if they are identical.
+
+const eqArrays = function(array1, array2) {
+  // First check to see if the arrays are the same length. If not return false.
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  // Make a loop that goes through each array and compares the 2 values at each index. If they do not match return false and end the loop.
+
+  for(let i = 0; i < array1.length; i++) {
+    if(array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+  
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
