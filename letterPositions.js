b1bc357693604ba65ce3a84675b7379e510e9b5e
Amit Baid <baidstar@hotmail.com>
2021-07-21 19:04:10 -0600

    function that returns the position of letters in a sentence

---
+++ letterPositions.js
@@ -0,0 +1,48 @@
+const eqArrays = function(array1, array2) {
+  // First check to see if the arrays are the same length. If not return false.
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  // Make a loop that goes through each array and compares the 2 values at each index. If they do not match return false and end the loop.
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+  
+};
+// Make a function that will console.log if an array is equal or not equal.
+const assertArraysEqual = function(actual, expected) {
+  // Run eqArrays on the data to get back a true or false value
+  let test = eqArrays(actual, expected);
+  // Next an if statement that will return a message based on true/false of the test
+  if (test === true) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+  
+
+};
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+  
+  for (let i = 0; i < sentence.length; i++) {
+   if (sentence[i] === " ") {}
+   else if (results[sentence[i]]) {
+     results[sentence[i]].push(i);
+    }else {
+      results[sentence[i]] = [i]
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions("lighthouse in the house"))
+
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
