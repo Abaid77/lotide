dc12cb375a7d9333a751728afcf64f85e76cde29
Amit Baid <baidstar@hotmail.com>
2021-07-26 16:31:25 -0600

    updated test to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,11 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-
-// TEST CODE
-
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [ 3, 4 ]);
+describe("#Middle", () => {
+  it("should return [3] with input of [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  })
+  it("should return [3, 4] with input of [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+  })
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,21 @@
-const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const assert = require('chai').assert;
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+  it("returns back 2 elements", () => {
+    assert.equal(tail(["Hello", "Lighthouse", "Labs"]).length, 2);
+  });
+  it("ensures the first element returned is Lighthouse", () => {
+    assert.equal(tail(["Hello", "Lighthouse", "Labs"])[0], "Lighthouse");
+  });
+  it("ensures the second element returned is Labs", () => {
+    assert.equal(tail(["Hello", "Lighthouse", "Labs"])[1], "Labs");
+  });
+  it("ensures the original array still has 3 elements", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.equal(words.length, 3);
+  });
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-console.log(words);
-assertEqual(words.length, 3); // original array should still have 3 elements!
+
+});
